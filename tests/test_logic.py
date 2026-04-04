b58f15523f0becc749a5f3ef5bcfe2e851d878fc
Kim Ngo <kim.c.ngo03@gmail.com>
2026-04-04 00:43:03 -0700

    Update test_logic.py

--- tests/test_logic.py
+++ tests/test_logic.py
@@ -1,1 +1,11 @@
-## include (unit tests for core functions)
+import logic as l
+
+print(l.choose_difficulty())
+
+print(l.display_change("a","applesauce","__________"))
+
+print(l.starting_display("applesauce"))
+
+l.art(30)
+
+print(l.validate("word","yes"))
