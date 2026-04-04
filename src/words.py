e00c477a1dd76720fbd6b685b01f13817336c6db
Kim Ngo <kim.c.ngo03@gmail.com>
2026-04-04 00:23:39 -0700

    Delete src/words.py

--- src/words.py
+++
@@ -1,7 +0,0 @@
-def word_list(difficulty):
-    if difficulty == "easy":
-        return ["car", "cat", "dog", "hat"]
-    elif difficulty == "medium":
-        return ["four", "make", "tire", "knew"]
-    else:
-        return ["create", "blender", "knowledge", "thrust"]
