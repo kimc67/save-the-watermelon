4b781cb3eb008c0548ac5de6fc9aaf7804290e5d
Kim Ngo <kim.c.ngo03@gmail.com>
2026-04-04 18:04:13 -0700

    Update test_logic.py

--- tests/test_logic.py
+++ tests/test_logic.py
@@ -6,7 +6,7 @@ print(l.choose_difficulty())
 # 1st paramter is the letter checked in the 2nd parameter, 3rd parameter is the starting display
 print(l.display_change("a","applesauce","__________"))
 
-# will convert every character of input into underscores
+# will convert every character of input into underscores, letters will reveal if more than 5 letters
 print(l.starting_display("applesauce"))
 
 # will print any number of watermelon slices
