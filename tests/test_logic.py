bb635988561fe168ab69894550917af6c0b5f7d3
Kim Ngo <kim.c.ngo03@gmail.com>
2026-04-04 17:57:53 -0700

    Update test_logic.py

--- tests/test_logic.py
+++ tests/test_logic.py
@@ -1,11 +1,17 @@
 import logic as l
 
+# will ask for user input
 print(l.choose_difficulty())
 
+# 1st paramter is the letter checked in the 2nd parameter, 3rd parameter is the starting display
 print(l.display_change("a","applesauce","__________"))
 
+# will convert every character of input into underscores
 print(l.starting_display("applesauce"))
 
+# will print any number of watermelon slices
 l.art(30)
 
+# 'word' in 1st parameter will validate 'yes' or 'no' in 2nd parameter
+# 'letter' in 1st parameter will validate an alphabet character in 2nd parameter
 print(l.validate("word","yes"))
